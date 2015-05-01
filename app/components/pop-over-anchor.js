b39c4dd63531f21e7316baeedf18299a97813039
Richard Livsey <richard@livsey.org>
2015-05-01 15:56:13 +0100

    Show pop-over on focus

--- app/components/pop-over-anchor.js
+++ app/components/pop-over-anchor.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
   classNames: "pop-over__anchor",
 
   popOver: null,
+  on:     "click",
 
   setupParent: Ember.on("init", function() {
     const parent = this.nearestWithProperty("isPopOver");
@@ -12,11 +13,26 @@ export default Ember.Component.extend({
 
   isOpen: Ember.computed.reads("popOver.isOpen"),
 
-  toggle: Ember.on("click", function(e) {
+  onClick: Ember.on("click", function(e) {
     e.preventDefault();
     e.stopPropagation();
 
+    if (this.get("on") !== "click") {
+      return;
+    }
+
     this.get("popOver").send("toggle");
+  }),
+
+  // show on focus
+  // TODO - how to handle blur? we don't necessarily want to auto-hide
+  //        on blur as otherwise it'll hide as soon as you interact with the popover
+  onFocus: Ember.on("focusIn", function() {
+    if (this.get("on") !== "focus") {
+      return;
+    }
+
+    this.get("popOver").send("open");
   })
 
 });
